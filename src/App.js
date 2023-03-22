60e93dd45dbaee44e479afb4bae4317f07177e97
Habtamu Asegahegn <34951324+HabtamuDes@users.noreply.github.com>
2023-03-22 05:42:37 -0700

    commit calander 2

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Form from "./scenes/form";
 //import Pie from "./scenes/pie";
 //import FAQ from "./scenes/faq";
 //import Geography from "./scenes/dashboard/geography";
-//import Calendar from "./scenes/calendar";
+import Calendar from "./scenes/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -30,13 +30,14 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/*<Route path="/bar" element={<Bar />} />*/}
               <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
+              {/*<Route path="/bar" element={<Bar />} />*/}
               {/*<Route path="/line" element={<Line />} />*/}
               {/*<Route path="/pie" element={<Pie />} />*/}
               {/*<Route path="/faq" element={<FAQ />} />*/}
               {/*<Route path="/geography" element={<Geography />} />*/}
-              {/*<Route path="/calendar" element={<Calendar />} />*/}
+             
             </Routes>
           </main>
         </div>
